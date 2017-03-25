f9569e0987ca1d47b6e1fcca57f59ce6b0fe54e0
dave <bianchini.davide.89@gmail.com>
2017-03-25 15:12:36 +0000

    hallo world

---
+++ src/main/java/com/dave/Application.java
@@ -0,0 +1,16 @@
+package com.dave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dave on 25/03/17.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main (String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
